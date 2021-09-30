31c0dc04d2026e9f0328c17ebc62e3951867c6d2
Muhammad Yasir Khan <yasirtariqg@gmail.com>
2021-09-30 19:46:35 +0500

    Initial Commit

---
+++ newProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newProject
+//
+//  Created by Muhammad Yasir Khan on 30/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
